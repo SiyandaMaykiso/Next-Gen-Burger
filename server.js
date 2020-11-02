97c18d0e4e1146d01eedd01ef2bf197864a0f760
Iamsiyanda <s.mayekiso@gmail.com>
2020-11-02 13:59:21 +0200

    Updates

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var path = require('path')
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+// Serves as the holder of the public directory in the application.
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+app.use(methodOverride('_method'))
+var exphbs = require('express-handlebars');
+app.engine('hbs', exphbs({
+    defaultLayout: 'main'
+}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+var routes = require('./controllers/burgers_controllers.js');
+app.use('/', routes);
+
+
+app.listen(PORT);
